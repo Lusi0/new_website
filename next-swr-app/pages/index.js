862afa9d697bda72b8833bce8fa08287e308cecc
CaelMcDermott <caelmcdermott@seattleacademy.org>
2021-10-22 12:39:08 -0700

    Update index.js

--- next-swr-app/pages/index.js
+++ next-swr-app/pages/index.js
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <video autoPlay loop muted className='w-full h-screen z-10'>
+      <video autoPlay muted className='w-full h-screen z-10'>
         <source src='lexvideo.mp4' type='video/mp4' />
       </video>
     </div>
