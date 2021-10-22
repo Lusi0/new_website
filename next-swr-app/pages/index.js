d8c5a41cc33c37f5bda84b55d54df5b4ed6fcabe
Lusi0 <ItsLusio@gmail.com>
2021-10-22 12:42:58 -0700

    sdfsadf
    trolling

--- next-swr-app/pages/index.js
+++ next-swr-app/pages/index.js
@@ -11,21 +11,6 @@ export default function Home() {
         <source src='lexvideo.mp4' type='video/mp4' />
       </video>
 
-      <div id="myNav" class="overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-
-
-        <div class="overlay-content">
-          <a href="#">About</a>
-          <a href="#">Services</a>
-          <a href="#">Clients</a>
-          <a href="#">Contact</a>
-        </div>
-
-      </div>
-
-
-      <span onclick="openNav()">open</span>
-    </div>  
+      
   )
 }
