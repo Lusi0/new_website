98c00041e574493969abba765e245865fa34ee36
Lusi0 <ItsLusio@gmail.com>
2021-10-22 14:38:33 -0700

    goodness gracious

---
+++ next-swr-app/pages/hider.js
@@ -0,0 +1,9 @@
+const tutorialelement = () => {
+  return (
+    <div>
+      <p>Knolage is power and I know a lot</p>
+    </div>
+  )
+}
+
+export default hider;
--- next-swr-app/pages/index.js
+++ next-swr-app/pages/index.js
@@ -1,16 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import hiding from '../components/hider.js'
+import Script from "next/script";
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <video autoPlay muted className='width:100% height:100%'>
+
+      <Script type="text/javascript" src="hider.js">
+
+      <video autoPlay muted onended="myFunction()" id="myDIV" className='width:100% height:100%'>
         <source src='lexvideo.mp4' type='video/mp4' />
       </video>
 
-      
+    </div>
   )
 }
