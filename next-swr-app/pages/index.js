4168da47af758e091abe370e4519403ee4e0faac
Lusi0 <ItsLusio@gmail.com>
2021-10-26 12:47:35 -0700

    Update index.js

--- next-swr-app/pages/index.js
+++ next-swr-app/pages/index.js
@@ -6,7 +6,7 @@ import Script from "next/script";
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
+//const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Home() {
   const [videoVisability, setVideoVisablity] = useState(true);
@@ -17,24 +17,22 @@ const hideVideo = () =>{
   setVideoVisablity(false)
 }
 
-const {data, error} = useSWR("https://api.coindesk.com/v1/bpi/currentprice.json", fetcher, {refreshInterval: 3000});
+//const {data, error} = useSWR("https://api.coindesk.com/v1/bpi/currentprice.json", fetcher, {refreshInterval: 3000});
 
-console.log({data})
+//console.log({data})
 
 useEffect(() => {
     setVideoVisablity(true)
-}, [data])
-
-
+}, [])
 
   return (
     <div className="max-w-7xl mx-auto">
 
-      <Hider>
+      {/* <Hider>
       <h1 className="text-center font-serif text-7xl">hello world</h1>
       <p>{videoVisability ? "foobar" : "closed"}</p>
       <p className="font-mono text-center text-7xl">{data?.bpi['USD'].rate}</p>
-      </Hider>
+      </Hider> */}
 
       <video autoPlay muted onEnded={() => hideVideo()} id="myDIV" className={videoVisability ? "block w-screen" : "hidden"} >
         <source src='lexvideo.mp4' type='video/mp4' />
